b477beb285cf80d869113081001b1e878c34ab60
Anna D <adetmold@gmail.com>
2020-09-21 18:13:19 -0700

    Update whitewater.py
    Final EDA and Regression analysis

--- whitewater.py
+++ whitewater.py
@@ -33,6 +33,9 @@ from sklearn.model_selection import train_test_split
 whitewater_accidents= pd.read_csv('/Users/AnnaD/Desktop/accident-2/accidents.csv')
 
 
+# I dropped accidents that didn't have a date (there weren't that many of them so I decided it was ok to drop them. As the date was key for analysis I decided that dropping the date, as opposed to imputing was sensible
+# Also, I dropped duplicate columns, as these were present and unecessary
+
 # In[3]:
 
 
@@ -55,14 +58,15 @@ whitewater_accidents['accidentdate'] =  pd.to_datetime(whitewater_accidents['acc
 
 
 whitewater_accidents.isna().sum()
-#drop columns I don't need & columns with high amounts of nan
-whitewater_accidents.drop(columns = ['contactname', 'contactphone','contactemail','othervictimnames','waterlevel','groupinfo','location'], inplace=True)
+#there are a lot of missing values
 
 
 # In[6]:
 
 
 whitewater_accidents.isna().sum()
+#drop columns I don't need & columns with high amounts of nan
+whitewater_accidents.drop(columns = ['contactname', 'contactphone','contactemail','othervictimnames','waterlevel','groupinfo','location'], inplace=True)
 
 
 # In[7]:
@@ -80,7 +84,7 @@ whitewater_accidents
 # In[9]:
 
 
-#filter USA Accidents
+#filter USA Accidents to map accidents
 whitewater_accidents_us = whitewater_accidents[whitewater_accidents['countryabbr']=='US']
 whitewater_accidents_us
 
@@ -108,6 +112,7 @@ fig.update_layout(
     geo_scope='usa',  # Plot only the USA instead of globe
 )
 fig.show()  # Output the plot to the screen
+#colorado had the highest number of accidents
 
 
 # In[12]:
@@ -145,35 +150,29 @@ whitewater_accidents[(whitewater_accidents['accidentdate'] > '1990-06-01') &
 
 
 whitewater_accidents.sort_values(by=['numvictims'], ascending = False)
-#8 is the maximum number of victims
+#8 is the maximum number of victims in one accident
 
 
 # In[16]:
 
 
-trainingData = whitewater_accidents.iloc[:, :].values
-dataset = whitewater_accidents.iloc[:, :].values
-
-
-# In[ ]:
-
-
-
+whitewater_accidents['age'] = whitewater_accidents['age'].fillna(method='ffill')
+#I wanted to make sure that there were no nans for age so I used the ffill method
+whitewater_accidents.info()
 
 
 # In[17]:
 
 
-whitewater_accidents['age'] = whitewater_accidents['age'].fillna(method='ffill')
-whitewater_accidents.info()
+fig = px.histogram(whitewater_accidents, x="age")
+fig.show()
+
+
 
 
 # In[18]:
 
 
-
-fig = px.histogram(whitewater_accidents, x="age")
-fig.show()
 #impute Nan for 0
 #babies don't go on whitewater very often, it must be that 0 was imputed for unknown ages
 cols=['age']
@@ -181,39 +180,22 @@ whitewater_accidents[cols] = whitewater_accidents[cols].replace({0:np.nan, 0:np.
 whitewater_accidents['age'] = whitewater_accidents['age'].fillna(method='ffill')
 
 
-
 # In[19]:
 
 
-whitewater_accidents
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+whitewater_accidents.isna().sum()
+#make sure most nans were removed.
 
 
 # In[20]:
 
 
-whitewater_accidents.isna().sum()
-
-
-# In[21]:
-
-
 whitewater_accidents.dropna(subset=['age'],inplace = True)
+#one row was removed, lacked age even after imputing
 whitewater_accidents.info()
 
 
-# In[22]:
+# In[21]:
 
 
 ##### visualize age of victims
@@ -222,17 +204,19 @@ fig = px.histogram(whitewater_accidents, x="age", range_x = (0,80), nbins = 20)
 
 fig.show()
 #25-29 most deaths
+#relatively normally distributed age of victims
 
 
-# In[23]:
+# In[22]:
 
 
 whitewater_accidents['type'].value_counts()
 
 
-# In[24]:
+# In[23]:
 
 
+#Wanted to change fatal and near miss to binary death or non death
 # Create a list to store the data
 death = []
 
@@ -260,7 +244,7 @@ whitewater_accidents['death'] = death
 whitewater_accidents
 
 
-# In[25]:
+# In[24]:
 
 
 # Create a list to store the data
@@ -322,42 +306,33 @@ whitewater_accidents['numeric_difficulty'] = numeric_difficulty
 whitewater_accidents
 
 
-# In[26]:
+# In[25]:
 
 
 whitewater_accidents['death'].value_counts()
 
 
-# In[27]:
+# In[26]:
 
 
+#check for class imbalance
 count_deaths = len(whitewater_accidents[whitewater_accidents['death']==0])
 count_no_death = len(whitewater_accidents[whitewater_accidents['death']==1])
 pct_of_no_death = count_no_death/(count_no_death+count_deaths)
 print("percentage of no deaths is", pct_of_no_death*100)
 pct_of_death = count_deaths/(count_no_death+count_deaths)
 print("percentage of deaths", pct_of_death*100)
+#Classes arent balanced but they aren't terrible
 
 
-# In[28]:
+# In[27]:
 
 
-whitewater_accidents['age'].isna().sum()
-
-
-# In[ ]:
-
-
-
-
-
-# In[29]:
-
-
+#created a column of month
 whitewater_accidents['month'] = pd.DatetimeIndex(whitewater_accidents['accidentdate']).month
 
 
-# In[30]:
+# In[28]:
 
 
 # Import label encoder
@@ -390,33 +365,27 @@ whitewater_accidents['death'] = whitewater_accidents['death'].astype(int)
 
 
 
-'''
-#difficulty
-whitewater_accidents['numeric_difficulty'] = whitewater_accidents['numeric_difficulty'].astype(int)
-whitewater_accidents['encoded_difficulty'] = label_encoder.fit_transform(whitewater_accidents['numeric_difficulty'])
 
-'''
-
-
-# In[31]:
+# In[29]:
 
 
 whitewater_accidents.info()
 
 
-# In[32]:
+# In[30]:
 
 
+#selected numeric columns to use as predictors
 whitewater_numeric_columns = whitewater_accidents[['age','privcomm','cause','month','encoded_experience','death']]
 
 
-# In[33]:
+# In[31]:
 
 
 whitewater_numeric_columns
 
 
-# In[34]:
+# In[32]:
 
 
 pd.crosstab(whitewater_numeric_columns.month,whitewater_numeric_columns.death).plot(kind='bar')
@@ -425,28 +394,30 @@ plt.xlabel('Month')
 plt.ylabel('Frequency of Death')
 
 
-# In[35]:
+# In[33]:
 
 
 whitewater_numeric_columns = whitewater_numeric_columns.rename(columns ={"death": "y"})
 
 
-# In[51]:
+# The idea was to try to see if the following columns (age','privcomm','cause','month','encoded_experience')  were a good predictor of fatalities. It seems like they are not a terrible predictor of fatalities. Thus, if we know the age of the participant, whether it was a private or commercial trip, the month and the experience of the participant we might be able to predict whether the accident is a fatal accident or not.
+
+# In[34]:
 
 
+#set X and y for analysis
 X = whitewater_numeric_columns.drop('y', axis = 1)
 y = whitewater_numeric_columns['y']
 
 
-# In[52]:
-
+# In[35]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 columns = X_train.columns
 
 
-# In[53]:
+# In[36]:
 
 
 from sklearn.linear_model import LogisticRegression
@@ -455,16 +426,17 @@ logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
 
 
-# In[54]:
+# In[37]:
 
 
 import statsmodels.api as sm
 logit_model=sm.Logit(y,X)
 result=logit_model.fit()
 print(result.summary2())
+#ensure p values are low
 
 
-# In[56]:
+# In[38]:
 
 
 logreg = LogisticRegression()
@@ -473,7 +445,7 @@ y_pred = logreg.predict(X_test)
 print('Accuracy of logistic regression classifier on test set:{:.2f}'.format(logreg.score(X_test, y_test)))
 
 
-# In[57]:
+# In[39]:
 
 
 from sklearn.metrics import confusion_matrix
@@ -483,8 +455,102 @@ print(confusion_matrix)
 
 # The result is telling us that we have 89+463 correct predictions and 23+181 incorrect predictions
 
-# In[58]:
+# In[40]:
 
 
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
+
+
+# Decision Tree Accuracy
+# I looked at decision trees briefly to see if the accuracy was different. The test accuracy wasn't very different
+
+# In[41]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+# Instantiate & fit the DT
+DT_model = DecisionTreeClassifier()
+DT_model.fit(X_train, y_train)
+
+
+
+# Evaluate its classification accuracy (Just on the training set for now)
+print(f"DT training set accuracy: {DT_model.score(X_train, y_train)}")
+print(f"DT test set accuracy: {DT_model.score(X_test, y_test)}")
+
+
+# In[42]:
+
+
+# Create a scaled version of X data
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# In[43]:
+
+
+# KNN - unscaled data
+from sklearn.neighbors import KNeighborsClassifier
+
+train_accs = []
+test_accs = []
+k_values = list(range(1,30))
+
+# Loop over different number of neighbors
+for k in k_values:
+
+    # Instantiate & fit
+    my_knn = KNeighborsClassifier(n_neighbors = k)
+    my_knn.fit(X_train, y_train)
+
+    # Evaluate on train & test data
+    train_accs.append( my_knn.score(X_train, y_train) )
+    test_accs.append( my_knn.score(X_test, y_test) )
+
+
+
+# KNN - scaled data
+
+train_accs_s = []
+test_accs_s = []
+k_values = list(range(1,30))
+
+# Loop over different number of neighbors
+for k in k_values:
+
+    # Instantiate & fit
+    my_knn = KNeighborsClassifier(n_neighbors = k)
+    my_knn.fit(X_train_scaled, y_train)
+
+    # Evaluate on train & test data
+    train_accs_s.append( my_knn.score(X_train_scaled, y_train) )
+    test_accs_s.append( my_knn.score(X_test_scaled, y_test) )
+
+
+
+# Plot the results
+plt.subplot(211) # plot unscaled classifier results
+plt.plot(k_values, train_accs, label='train')
+plt.plot(k_values, test_accs, label='test')
+plt.legend()
+plt.xlabel('number of neighbors')
+plt.ylabel('accuracy')
+plt.title('KNN Accuracy (UNscaled data)')
+
+plt.subplot(212) # plot scaled classifier results
+plt.plot(k_values, train_accs_s, label='train')
+plt.plot(k_values, test_accs_s, label='test')
+plt.legend()
+plt.xlabel('number of neighbors')
+plt.ylabel('accuracy')
+plt.title('KNN Accuracy (SCALED data)')
+
+plt.tight_layout()
+plt.show()
